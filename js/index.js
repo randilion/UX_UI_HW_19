aab6ced76b085b6f578b5f904c0cc7a39373e1f9
randilion <117680252+randilion@users.noreply.github.com>
2023-04-26 07:38:59 -0500

    javascript

---
+++ js/index.js
@@ -0,0 +1,30 @@
+console.log("hello world");
+
+  function mouseOver() {
+    document.getElementById("demo").style.color = "#DD2FED";
+  }
+
+  function mouseOut() {
+    document.getElementById("demo").style.color = "#8B8B8B";
+  }
+  var tabs = $('.tabs');
+  var selector = $('.tabs').find('a').length;
+  //var selector = $(".tabs").find(".selector");
+  var activeItem = tabs.find('.active');
+  var activeWidth = activeItem.innerWidth();
+  $(".selector").css({
+    "left": activeItem.position.left + "px", 
+    "width": activeWidth + "px"
+  });
+  
+  $(".tabs").on("click","a",function(e){
+    e.preventDefault();
+    $('.tabs a').removeClass("active");
+    $(this).addClass('active');
+    var activeWidth = $(this).innerWidth();
+    var itemPos = $(this).position();
+    $(".selector").css({
+      "left":itemPos.left + "px", 
+      "width": activeWidth + "px"
+    });
+  });
